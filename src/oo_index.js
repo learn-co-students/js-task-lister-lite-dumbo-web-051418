b61f89f46fc71ae44d1498548f77e31cc04a24f0
Sammy Akharaz <ketrixz@gmail.com>
2018-07-01 22:35:16 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,20 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+
+  const form = document.querySelector('#create-task-form')
+
+  form.addEventListener('submit',addToDo)
+
+  function addToDo(e) {
+    e.preventDefault()
+    // console.log('adding list')
+    const taskDescription = document.querySelector('#new-task-description').value
+    // console.log(taskDescription)
+    const li = document.createElement('li')
+    li.innerHTML = taskDescription
+    document.querySelector('#tasks').appendChild(li)
+
+  }
+
 });
